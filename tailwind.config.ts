a1f249540423bba63e1aac3be2836e5c7f4649d4
Shaun O'Neill <shaunoneill7@gmail.com>
2025-04-18 22:14:56 +0100

    Update tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss"
 
 const config = {
-  darkMode: ["class"],
+  darkMode: "selector",
   content: [
     "./src/pages/**/*.{ts,tsx}",
     "./src/components/**/*.{ts,tsx}",
